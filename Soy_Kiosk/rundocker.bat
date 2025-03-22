05646f924b5a58f9b692a94037650886142ce701
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-22 18:49:54 -0400

    sdfa

--- Soy_Kiosk/rundocker.bat
+++ Soy_Kiosk/rundocker.bat
@@ -1,1 +1,1 @@
-docker run --rm --privileged -v ~/.docker:/root/.docker   ghcr.io/home-assistant/amd64-builder:latest --all -t . -r https://github.com/Soy-HA/HA-Addons -b main 
+ docker run --rm --privileged -v ~/.docker:/root/.docker   ghcr.io/home-assistant/amd64-builder:latest --all -t Soy_Kiosk -r https://github.com/Soy-HA/HA-Addons/ -b main
