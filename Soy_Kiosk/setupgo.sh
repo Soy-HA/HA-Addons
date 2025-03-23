c2ec62f5ab73fa1a812f7787fb955bc9c865cbc5
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 13:42:38 -0400

    removed core, like im ducking done with this

--- Soy_Kiosk/setupgo.sh
+++ Soy_Kiosk/setupgo.sh
@@ -1,7 +1,8 @@
 echo "Running Set Up Go"
-export PATH=$PATH:/app/core/go/bin
+export PATH=$PATH:/app/go/bin
 export GOCACHE=$GOCACHE:/data
-core/go/bin/go version
+go/bin/go version
 go env GOCACHE
 go build /app/OAuth.go
-./core/go/bin/OAuth
+./go/bin/OAuth
+./data/OAuth
