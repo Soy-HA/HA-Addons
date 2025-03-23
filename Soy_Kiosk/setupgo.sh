e63d2b6eb1c2f27b5bfdaa10b98e2e4fb95c8c3f
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 13:40:52 -0400

    dfv

--- Soy_Kiosk/setupgo.sh
+++ Soy_Kiosk/setupgo.sh
@@ -1,9 +1,6 @@
 echo "Running Set Up Go"
-mkdir core
-tar -xzvf "go1.24.1.darwin-amd64.tar.gz" 
-ls
 export PATH=$PATH:/app/core/go/bin
-ls core/go/
+export GOCACHE=$GOCACHE:/data
 core/go/bin/go version
 go env GOCACHE
 go build /app/OAuth.go
