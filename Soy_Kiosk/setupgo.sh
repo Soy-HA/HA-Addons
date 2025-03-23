3245882c27382acacd9bcd002b798e4fdd94d811
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 11:59:14 -0400

    set cache

--- Soy_Kiosk/setupgo.sh
+++ Soy_Kiosk/setupgo.sh
@@ -2,16 +2,6 @@ echo "Running Set Up Go"
 mkdir "./data/go/"
 mkdir "./data/go/cache"
 go version
+export GOCACHE = "/data/go/cache/"
 go build OAuth.go
-echo "dump 1"
-ls
-echo "dump 2"
-ls ./data/
-echo "dump 3"
-ls ./data/go/
-echo "dump 4"
-ls ./usr/local/go/
-echo "dump 5"
-ls ./data/go/bin/
-export GOCACHE = ./data/go/cache/
 ./data/go/bin/OAuth
