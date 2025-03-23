e13349991799d77fb08b4d37eebd195d80a3356c
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 13:23:42 -0400

    lol downloaded a version without a bin

--- Soy_Kiosk/setupgo.sh
+++ Soy_Kiosk/setupgo.sh
@@ -1,7 +1,8 @@
 echo "Running Set Up Go"
 tar -xzf "go1.24.1.src.tar.gz" /app/core/
 export PATH=$PATH:/app/core/go/bin
-/app/core/go version
+ls /app/core/go/
+/app/core/go/bin/go version
 go env GOCACHE
 go build /app/OAuth.go
 ./app/core/go/bin/OAuth
