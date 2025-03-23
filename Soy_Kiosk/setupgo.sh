58ff43121da20fde5ed5405a141518548d52a84c
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 13:47:44 -0400

    cleanup

--- Soy_Kiosk/setupgo.sh
+++ Soy_Kiosk/setupgo.sh
@@ -1,7 +1,9 @@
 echo "Running Set Up Go"
-export PATH=$PATH:/app/go/bin
-export GOCACHE=$GOCACHE:/data
-go/bin/go version
+export PATH=$PATH:/app/go/bin/
+export GOCACHE=$GOCACHE:/data/
+echo "go version" 
+go version
+echo "go gocache"
 go env GOCACHE
 go build /app/OAuth.go
 ./go/bin/OAuth
