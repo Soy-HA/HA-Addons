2050bba9f20a7445e1fd039bbd9765a34e98e7ff
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 17:47:51 -0400

    added config file access

--- Soy_Kiosk/OAuth.go
+++ Soy_Kiosk/OAuth.go
@@ -4,6 +4,8 @@ import (
     "fmt"
 	"net/http"
 	"log"
+	"encoding/json"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -12,10 +14,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func oAuthHandler(w http.ResponseWriter, r *http.Request) {
+	//See if local ip
+	//use ip, _, err := net.SplitHostPort(r.RemoteAddr)
+
+
     title := r.URL.Path[len("/Kiosk/"):]
 	queryParams := r.URL.Query()
-	
-	
+	dashboard := queryParams["dashboard"]
+	if( dashboard == nil ) {
+		//change this to pull in from config file
+		content, err := os.ReadFile("/data/options.json")
+		var data Data
+		err = json.Unmarshal(content, &data)
+		dashboard := data.default_dashboard
+	}
     fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", title, queryParams["dashboard"])
 }
 
--- Soy_Kiosk/setupgo.sh
+++ Soy_Kiosk/setupgo.sh
@@ -5,4 +5,5 @@ export GOCACHE=$GOCACHE:/data
 go version
 echo "build oauth.go"
 GOCACHE=/data/ go build OAuth.go
+echo "Built, starting"
 ./OAuth
