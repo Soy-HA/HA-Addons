4e5c46c017c2a9df5ea6479c4098353d590815c8
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 13:33:53 -0400

    changing paths

--- Soy_Kiosk/setupgo.sh
+++ Soy_Kiosk/setupgo.sh
@@ -1,9 +1,9 @@
 echo "Running Set Up Go"
 mkdir core
-tar -xzf "go1.24.1.darwin-amd64.tar.gz" /app/core/
+tar -xzf "go1.24.1.darwin-amd64.tar.gz" core/
 export PATH=$PATH:/app/core/go/bin
-ls /app/core/go/
-/app/core/go/bin/go version
+ls core/go/
+core/go/bin/go version
 go env GOCACHE
 go build /app/OAuth.go
-./app/core/go/bin/OAuth
+./core/go/bin/OAuth
