f244617fcba0c07a142ce590d1accca0de00df99
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 12:54:32 -0400

    hahahahahaha

--- Soy_Kiosk/setupgo.sh
+++ Soy_Kiosk/setupgo.sh
@@ -1,12 +1,8 @@
 echo "Running Set Up Go"
 mkdir "./data/go/"
 mkdir "./data/go/cache"
-./main
+./go/out/main
 go version
-pwd
-go env GOCACHE
-GOCACHE = /data/go/cache/
-export GOCACHE
 go env GOCACHE
 go build OAuth.go
 ./data/go/bin/OAuth
