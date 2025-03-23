7d010df9c7c14ced1190cdf84d237cb996da7dd7
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 09:58:33 -0400

    Trying to get go

---
+++ Soy_Kiosk/OAuth.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func oAuthHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/OAuth/"):]
+    p, err := loadPage(title)
+	if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/OAuth/", oAuthHandler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
