3adf0d4b041e9855f4b8e3bb5be2e539cbcaad75
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 16:43:36 -0400

    where are you

--- Soy_Kiosk/setupgo.sh
+++ Soy_Kiosk/setupgo.sh
@@ -1,7 +1,9 @@
 echo "Running Set Up Go"
 export PATH=$PATH:/app/go/bin
+export GOPATH=$GOPATH:/app/go/bin
 export GOCACHE=$GOCACHE:/data
 go version
 echo "build oauth.go"
 GOCACHE=/data/ go build OAuth.go
-./go/bin/OAuth
+./GOPATH/OAuth
+./OAuth
