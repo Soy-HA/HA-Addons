3b2bf7683759bd087f081649f1aac5d254eeb173
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-23 21:05:52 -0400

    Added Auth Check

--- Soy_Kiosk/OAuth.go
+++ Soy_Kiosk/OAuth.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"encoding/json"
 	"os"
+	"io"
 )
 
 type haConfig struct {
@@ -41,9 +42,32 @@ func oAuthHandler(w http.ResponseWriter, r *http.Request) {
 		dashboard = data.default_dashboard
 	}
 	//Authethenicate user
+	access_token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2YzQ4MmI1MTIxZmU0NGVhOTM2ZDQxOTRkMWEzZWExZSIsImlhdCI6MTc0Mjc3Nzk5NSwiZXhwIjoyMDU4MTM3OTk1fQ.MlljcluzEDUmMQgGoB6-IjOu16aJmB-MuMF2_nNgTLA"
+	url := "http://homeassistant.local:8123/api/websocket"
+	
+	
+	var jsonStr = []byte(`{"type": "auth", "access_token": "` + access_token + `"}`)
+	erq, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	//req.Header.Set("X-Custom-Header", "myvalue")
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := io.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+	
+	
+	
 	//Send Dashboard
 		//TEMP for funsies. Just seeing what happens if I jsut throw the dashboard in here lololol
-	url := "http://homeassistant.local:8123/dashboard-kiosk/" // Replace with the desired URL
+	url := "http://homeassistant.local:8123/" // Replace with the desired URL
 
 	fmt.Fprintf(w,"<iframe src='%s' title='dashboard' id='dashboard' name='dashboard'></iframe>",url)
 	
