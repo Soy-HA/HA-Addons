9fb037f4d08a29e2d666ebb7a1468a76d46538ad
Soywatman <62768968+Soywatman@users.noreply.github.com>
2025-03-25 22:37:08 -0400

    IGiveUpYouWinIWon'tTryHidingYourStupidDuckingSidebarHomeAssistnat

--- Soy_Kiosk/setupgo.sh
+++
@@ -1,9 +0,0 @@
-echo "Running Set Up Go"
-export PATH=$PATH:/app/go/bin
-export GOPATH=$GOPATH:/app/go/bin
-export GOCACHE=$GOCACHE:/data
-go version
-echo "build oauth.go"
-GOCACHE=/data/ go build -tags debug OAuth.go
-echo "Built, starting"
-./OAuth
